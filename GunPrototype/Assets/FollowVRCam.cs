69364ab96654c13b9a5ce6022ea3545cbfcbc977
Gijs Bakker <gijs.bakker@student.hku.nl>
2019-11-01 14:22:41 +0100

    alright nice we now know how we can attach something to the players body :). and follow it.

---
+++ GunPrototype/Assets/FollowVRCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowVRCam : MonoBehaviour
+{
+    public Transform VRCamera;
+    float xOffset;
+
+    private void Start()
+    {
+        xOffset = transform.localPosition.x - VRCamera.localPosition.x;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.localPosition = new Vector3(VRCamera.localPosition.x - xOffset, transform.localPosition.y, VRCamera.localPosition.z);
+    }
+}
