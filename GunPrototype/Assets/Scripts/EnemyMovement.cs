61d06c1b6c183043b9f21c0d530524f273e4fe38
Gijs Bakker <gijs.bakker@student.hku.nl>
2019-12-05 14:09:35 +0100

    Finally found a pretty good way to do animations even with locomotion.

---
+++ GunPrototype/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class EnemyMovement : MonoBehaviour
+{
+    private Animator animator;
+
+    public float inputX;
+    public float inputY;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        inputY = Input.GetAxis("Vertical");
+        inputX = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("InputY", inputY);
+    }
+}
