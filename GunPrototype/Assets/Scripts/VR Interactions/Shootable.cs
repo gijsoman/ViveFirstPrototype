3ab649a30f215af6f4a7eb8375d746661fc215fc
Gijs Bakker <gijs.bakker@student.hku.nl>
2020-01-09 16:46:38 +0100

    Framerate dipped massively for some reason. Need to figure out why.

--- GunPrototype/Assets/Scripts/VR Interactions/Shootable.cs
+++ GunPrototype/Assets/Scripts/VR Interactions/Shootable.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 using Valve.VR.InteractionSystem;
 using Valve.VR;
 
-[RequireComponent(typeof(Interactable))]
+[RequireComponent(typeof(Interactable), typeof(AudioSource))]
 public class Shootable : MonoBehaviour
 {
     public SteamVR_Action_Boolean Shoot;
     public Transform EndOfBarrel;
     public AudioClip ShootSound;
+    public GameObject muzzleFlashPrefab;
+
+    private AudioSource audiouSource;
+
+    private void Start()
+    {
+        audiouSource = GetComponent<AudioSource>();
+    }
 
     private void HandAttachedUpdate(Hand hand)
     {
-        //Debug.Log();
+        
         if (Shoot != null && Shoot.GetStateDown(hand.handType))
         {
             Vector3 forward = EndOfBarrel.transform.TransformDirection(Vector3.right) * 10;
             ObjectPooler.Instance.SpawnFromPool("Bullet", EndOfBarrel.position, Quaternion.LookRotation(forward));
+            GameObject tempFlash;
+            tempFlash = Instantiate(muzzleFlashPrefab, EndOfBarrel.position, Quaternion.LookRotation(forward));
+            audiouSource.PlayOneShot(ShootSound);
+            Destroy(tempFlash, 0.5f);
         }
     }
 
